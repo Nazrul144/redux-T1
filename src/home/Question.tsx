bbaeb94ae1381648fba090f87dd72951de48a4bd
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-07-03 16:41:12 +0600

    Card designed completed

--- src/home/Question.tsx
+++ src/home/Question.tsx
@@ -1,11 +1,27 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useAppSelector } from "../redux/hooks";
+import { Button } from "@/components/ui/button";
 
 const Question = () => {
   const { value } = useAppSelector((state) => state.quiz);
-  console.log(value);
+  const quizCard = value[0];
   return (
-    <div>
-      
+    <div className="flex justify-center">
+      <Card className="w-[450px]">
+        <CardHeader>
+          <CardTitle>{quizCard.question}</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div>
+            {quizCard.options?.map((option, index) => (
+              <Button key={index} size="lg" className="w-full mt-3">
+                {option}
+              </Button>
+            ))}
+          </div>
+          <h1 className="font-black mt-2">Correct Answer: {quizCard.correctAnswer}</h1>
+        </CardContent>
+      </Card>
     </div>
   );
 };
