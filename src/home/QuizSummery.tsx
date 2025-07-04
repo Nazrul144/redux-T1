58bddb114de4216f272b21d5a924caad5533d4f3
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-07-04 17:12:18 +0600

    Progress bar added

--- src/home/QuizSummery.tsx
+++ src/home/QuizSummery.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Progress } from "@radix-ui/react-progress";
 
 function QuizSummery() {
   return (
@@ -9,6 +10,20 @@ function QuizSummery() {
         </CardHeader>
         <CardContent>
           <h3>You got 4 out of 10</h3>
+          {/*Progress bar*/}
+          <Progress value={33} />
+          <div>
+            <span>40%</span>
+            <span>Performance: Good</span>
+          </div>
+          <div>
+            <p>
+                <strong>Incorrect Answer: 5</strong>
+            </p>
+          </div>
+          <div>
+            <p>Great job! Keep practicing.</p>
+          </div>
         </CardContent>
       </Card>
     </div>
